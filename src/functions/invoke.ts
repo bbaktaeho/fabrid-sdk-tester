5d2a2157a71dcd4ed91cb9a8ad29dd34c54910bb
bbaktaeho <lth9036@gmail.com>
2021-08-26 14:45:18 +0900

    Update: invoke & query

---
+++ src/functions/invoke.ts
@@ -0,0 +1,64 @@
+import { Gateway, Wallets } from "fabric-network";
+import path from "path";
+import fs from "fs";
+import { IEnv } from "../interfaces/IEnv";
+import { Env } from "../config";
+
+export async function invoke({ ccpPath }: IEnv, asLocalhost: boolean) {
+  const clientId = Env.CLIENT_ID;
+  const channel = Env.CHANNEL;
+  const chaincodeName = Env.CHAINCODE_NAME;
+
+  try {
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(clientId);
+    if (!identity) {
+      console.log(
+        'An identity for the user "appUser" does not exist in the wallet'
+      );
+      console.log("Run the registerUser.ts application before retrying");
+      return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: clientId,
+      discovery: { enabled: true, asLocalhost },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork(channel);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chaincodeName);
+
+    // todo: your ctor of contract
+    const args = {
+      args: [
+        "-",
+        "0x5e11a44C6139f3D6aD295e7Ec706fe362f42518f",
+        "100000000000000000000000000000",
+      ],
+    };
+    const jsonArgs = JSON.stringify(args);
+    await contract.submitTransaction(
+      "AdminOperations",
+      "SetBalanceOf",
+      jsonArgs
+    );
+    console.log(`Transaction has been submitted`);
+    await gateway.disconnect();
+  } catch (error) {
+    console.error(`Failed to submit transaction: ${error}`);
+    process.exit(1);
+  }
+}
---
+++ src/functions/query.ts
@@ -0,0 +1,59 @@
+import { Gateway, Wallets } from "fabric-network";
+import path from "path";
+import fs from "fs";
+import { IEnv } from "../interfaces/IEnv";
+import { Env } from "../config";
+
+export async function query({ ccpPath }: IEnv, asLocalhost: boolean) {
+  const clientId = Env.CLIENT_ID;
+  const channel = Env.CHANNEL;
+  const chaincodeName = Env.CHAINCODE_NAME;
+
+  try {
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(clientId);
+    if (!identity) {
+      console.log(
+        'An identity for the user "appUser" does not exist in the wallet'
+      );
+      console.log("Run the registerUser.ts application before retrying");
+      return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: clientId,
+      discovery: { enabled: true, asLocalhost },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork(channel);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chaincodeName);
+
+    // todo: your ctor of contract
+    const args = { args: ["-", "0x5e11a44C6139f3D6aD295e7Ec706fe362f42518f"] };
+    const jsonArgs = JSON.stringify(args);
+    const result = await contract.evaluateTransaction(
+      "AdminOperations",
+      "GetBalanceOf",
+      jsonArgs
+    );
+    console.log(
+      `Transaction has been evaluated, result is: ${result.toString()}`
+    );
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    process.exit(1);
+  }
+}
