5d7ba4815a811ceda851b8521cec7bff48c414a0
bbaktaeho <lth9036@gmail.com>
2021-09-10 15:54:39 +0900

    Update: script

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Env } from "./config";
 import { enrollAdmin } from "./functions/enrollAdmin";
 import { invoke } from "./functions/invoke";
+import { sleep } from "./utils/sleep";
 import { query } from "./functions/query";
 import { registerUser } from "./functions/registerUser";
-import { sleep } from "./utils/sleep";
 
 const ccpPath = Env.CCP_PATH;
 const certificateAuthorities = Env.CERTIFICATE_AUTHORITIES;
@@ -14,7 +14,10 @@ const asLocalhost = Env.AS_LOCALHOST;
 (async () => {
   await enrollAdmin({ ccpPath, adminId, certificateAuthorities, mspId });
   await registerUser({ ccpPath, adminId, certificateAuthorities, mspId });
-  await invoke({ ccpPath, adminId, certificateAuthorities, mspId }, asLocalhost);
+  await invoke(
+    { ccpPath, adminId, certificateAuthorities, mspId },
+    asLocalhost
+  );
   await sleep(2);
   await query({ ccpPath, adminId, certificateAuthorities, mspId }, asLocalhost);
 })();
